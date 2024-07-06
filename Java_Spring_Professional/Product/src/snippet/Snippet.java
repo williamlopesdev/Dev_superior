42f567f01f94e7590215fcb3a35396a355df3fe8
williamlopesdev <wilcypherr@gmail.com>
2024-07-06 14:37:56 -0300

    revisão de OO e SQL com Java e JDBC

---
+++ Java_Spring_Professional/Product/src/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Snippet {
+	Qual abordagem abaixo tem como principal objetivo fazer a compilação (empacotamento) do conteúdo web em tempo de construção deixando a distribuição do conteúdo estática e mais performática?
+	*
+	1 ponto
+	SSR
+	SPA
+	SSG
+	Isomorphic JS
+	ReactJS
+}
+
